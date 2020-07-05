7d3cf1092cd25b82df0ed634f78a2f083182c438
jorgelfant <jorgel_fant@yahoo.com>
2020-07-05 18:25:16 +0200

    enfin putain !

--- src/main/java/com/example/couponservice/repos/CouponRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.couponservice.repos;
-
-import com.example.couponservice.model.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponRepo extends JpaRepository<Coupon,Long> {
-    Coupon findByCode(String code);
-}
---
+++ src/test/java/com/igs/springcloud/CouponserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.igs.springcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+class CouponserviceApplicationTests {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
